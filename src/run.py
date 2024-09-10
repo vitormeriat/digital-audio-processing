3b3b68e7fede16d8a784bcba5de6a777199633dd
Vitor Meriat <vitormeriat@gmail.com>
2024-09-10 23:53:33 +0100

    Add poetry and update notebooks

---
+++ src/run.py
@@ -0,0 +1,11 @@
+import skimage
+import numpy as np
+import pandas as pd
+from skimage import io
+import librosa#, librosa.display
+import matplotlib.pyplot as plt
+
+
+print(librosa.__version__)
+
+
